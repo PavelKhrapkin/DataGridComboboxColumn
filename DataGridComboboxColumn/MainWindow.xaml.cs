32f6dc033272e3d7cecacad202f9237bd30d0393
Павел Храпкин <Khrapkin.P@csitltd.ru>
2017-09-29 20:16:31 +0300

    работающий макет DataGrid

--- DataGridComboboxColumn/MainWindow.xaml.cs
+++ DataGridComboboxColumn/MainWindow.xaml.cs
@@ -15,7 +15,7 @@ using System.Windows.Shapes;
 
 namespace DataGridComboboxColumn
 {
-    /// <summary>
+    /// <summary> 
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
@@ -23,6 +23,50 @@ namespace DataGridComboboxColumn
         public MainWindow()
         {
             InitializeComponent();
+            List<CalendarItem> items = new List<CalendarItem>()
+            {
+                new CalendarItem(3, "Work"),
+                new CalendarItem(2, "travel"),
+                new CalendarItem(1, "vacation"),
+                new CalendarItem(6, "Fishing")
+            };
+            
+            dataGrid1.ItemsSource = items;
+
+            Days.ItemsSource = DayOfWeek.days;
         }
+
+        private void dataGrid1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+
+        private void DayChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var d = (sender as ComboBox).SelectedItem;
+        }
+    }
+    class CalendarItem
+    {
+        public DayOfWeek Day { get; set; }
+        public string Text { get; set; }
+
+        public CalendarItem(int i, string txt)
+        {
+            Day = new DayOfWeek(i);
+            Text = txt; ;
+        }
+    }
+
+    public class DayOfWeek
+    {
+        public static readonly List<string> days = new List<string>()
+        { "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+
+        public int Id { get; set;  }
+        public string Name { get; set; }
+
+        public DayOfWeek(int i)
+        { Id = i;  Name = days[i - 1]; }
     }
 }
